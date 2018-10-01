8badb71bfa11a3ddc96d2bb33a5adcd70062edfe
Dan Uluma <danuluma@gmail.com>
2018-10-01 14:09:20 +0300

    [Feature #160883915] added method for user to log in

--- users.py
+++ users.py
@@ -1,24 +1,39 @@
+from userdata import Userdata
 class User:
-	"""Creates variables for any user"""
+  """Creates variables for any user"""
 
-	def __init__(self):
-		self.username = ""
-		self.password = ""
-		self.access_level = 0
+  def __init__(self):
+    self.username = ""
+    self.password = ""
+    self.access_level = 0
 
-	
-	def create_account(self):
-		print("Register to create an account")
-		self.username = input("Enter a username: ")
-		self.password = input("Enter a password: ")
 
-		registered_user = {
-			'username': self.username,
-			'password': self.password
-		}
+  def create_account(self):
+    print("Register to create an account")
+    self.username = input("Enter a username: ")
+    self.password = input("Enter a password: ")
 
-		from userdata import Userdata
-		user_details = Userdata().user_data()
-		user_details.append(registered_user)
-		print(user_details)
-		return "Hoorah! You have created an account you can now log in"
+    registered_user = {
+    	'username': self.username,
+    	'password': self.password
+    }
+
+    user_details = Userdata().user_data()
+    user_details.append(registered_user)
+    print(user_details)
+    return "Hoorah! You have created an account you can now log in"
+
+  def user_login(self):
+    self.username = input("Enter a username: ")
+    self.password = input("Enter a password: ")
+    user_data = Userdata().user_data()
+
+    user = [user for user in user_data if user_data['username']==self.username]
+    if user[0]['password'] == self.password:
+      print("Logged in!")
+      login_details = {
+      "logged_in": True,
+      "access_level": 0,
+      "Timestamp":"Coming soon"
+      }
+      Userdata().user_login_data().append(login_details)
