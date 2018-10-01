23988dff677fdee746ea2756724b316a06ee572a
codeplus254 <ronnymageh@gmail.com>
2018-10-01 13:50:33 +0300

    Feature #160884139 A moderatorcan delete a comment given a comment_id

---
+++ moderator.py
@@ -0,0 +1,7 @@
+
+Class Moderators(User):
+    """This python module defines moderator access.  """
+    def remove_comment(comment_id):
+        """THe moderator can delete inappropriate comments after identifying their id"""
+        del comment_list[comment_id]
+        return comment_list
