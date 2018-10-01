6341efd8a378061571eae02987bca0c414742a54
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-10-01 12:40:49 +0300

    [ Feature #160884900 ] Finish sign up
    The user can now sign up with a username and a pass word

---
+++ userdata.py
@@ -0,0 +1,7 @@
+Class Userdata():
+
+	def __init__(self):
+		self.userdetails = []
+
+	def user_data(self):
+		return self.userdetails
--- users.py
+++ users.py
@@ -1,9 +1,23 @@
 Class User:
 	"""Creates variables for any user"""
 
-	def __init__(self, username, password):
-		self.username = username
-		self.password = password
+	def __init__(self):
+		self.username = ""
+		self.password = ""
 		self.access_level = 0
 
 	
+	def create_account(self):
+		print("Register to create an account")
+		self.username = input("Enter a username: ")
+		self.password = input("Enter a password: ")
+
+		registered_user = {
+			'username': self.username,
+			'password': self.password
+		}
+
+		from .userdata import user_data
+		user_details = user_data()
+		userdetails.append(registered_user)
+		print("Hoorah! You have created an account you can now log in")
