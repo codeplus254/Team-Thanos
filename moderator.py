bfe07549b6861b76b24a054a587e9c860fe7b42b
codeplus254 <ronnymageh@gmail.com>
2018-10-01 15:19:54 +0300

    Chore  #160884214 added defined init in moderator class

--- moderator.py
+++ moderator.py
@@ -1,6 +1,8 @@
 
 Class Moderators(User):
     """This python module defines moderator access.  """
+    def __init__(self):
+        self.access_level = 1
     def remove_comment(comment_id):
         """The moderator can delete inappropriate comments after identifying their id"""
         del comment_list[comment_id]
